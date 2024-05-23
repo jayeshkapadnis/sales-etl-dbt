e7d8571c10e67790c127889719fa38e2cf06e5da
Jayesh <jayesh.kapadnis@linxhealth.com>
2024-05-23 21:54:49 +0530

    Sample dbt core project for etl of sales data

---
+++ models/sales_data/transform_sales_data.sql
@@ -0,0 +1,19 @@
+WITH sales_data AS (
+    SELECT * FROM {{ ref('stg_sales_data') }}
+),
+
+constants AS (
+    SELECT * FROM {{ ref('stg_constants') }}
+)
+
+SELECT
+    sd.sale_id,
+    c.constant_name AS product_name,
+    sd.quantity,
+    sd.sale_date
+FROM
+    sales_data sd
+JOIN
+    constants c
+ON
+    sd.product_id = c.constant_value;
