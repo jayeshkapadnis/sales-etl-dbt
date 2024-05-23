4a41de88c8244fd56fdd70e736c82145e88ef187
Jayesh <jayesh.kapadnis@linxhealth.com>
2024-05-23 22:16:00 +0530

    Correct names of source tables

--- models/sales_data/transform_sales_data.sql
+++ models/sales_data/transform_sales_data.sql
@@ -1,9 +1,9 @@
 WITH sales_data AS (
-    SELECT * FROM {{ ref('stg_sales_data') }}
+    SELECT * FROM {{ ref('sales_data') }}
 ),
 
 constants AS (
-    SELECT * FROM {{ ref('stg_constants') }}
+    SELECT * FROM {{ ref('constants') }}
 )
 
 SELECT
